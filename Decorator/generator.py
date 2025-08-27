acacc252a0a8aa586b3c0dd9ac88ec5964007447
sinan-prvt <mohamedsinan9400@gmail.com>
2025-08-27 13:04:05 +0530

    updated12

--- Decorator/generator.py
+++ Decorator/generator.py
@@ -24,3 +24,17 @@
 # gen = Counter()
 # for i in range(5):
 #     print(next(gen))
+
+
+
+
+#------ fibonnaci
+
+
+# def fibo(n):
+#     a,b = 0,1
+#     for _ in range(n):
+#         yield a
+#         a,b = b, a+b
+# for num in fibo(10):
+#     print(num)
