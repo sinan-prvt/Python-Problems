f0c3108b6d7d7003f05194a378799c0c3102ab2c
sinan-prvt <mohamedsinan9400@gmail.com>
2025-10-23 11:50:15 +0530

    Dictionary-Comprehesion-added

---
+++ Comprehesion/DictComprehension.py
@@ -0,0 +1,24 @@
+
+#--------------------------------- Dictionary Comprehension -------------------------------------------
+
+
+# number = [1,2,3,4,5,6]                                               # Find Square 
+# square = {n: n*n for n in number}
+# print(square)
+
+
+# even = {n: "Even" if n % 2 == 0 else "Odd" for n in range(5)}         # Conditional
+# print(even)
+
+
+# number = [1,2,3,4,5,6,7]                                              # Filtering
+# even_sqr = {n: n**2 for n in number if n % 2 == 0}
+# print(even_sqr)
+
+
+
+# word = ["sinan", "Mohamed", "Tharammal"]                              # Transforming strings
+# length = {n: len(n) for n in word}
+# print(length)
+
+
