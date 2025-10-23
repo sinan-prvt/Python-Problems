f92e04e8f7c632e2c5ef79b2579599b389ec2629
sinan-prvt <mohamedsinan9400@gmail.com>
2025-10-23 14:02:56 +0530

    "Tuple-Updated"

--- DataTypes/tuple.py
+++ DataTypes/tuple.py
@@ -1,16 +1,50 @@
 #-------------------------------------------- Tuple ------------------------------------------------
 
-
-# n = ("red",10, 1.29, True)
+# n = ("red",10, 1.29, True)                          # Basic Example
 # print(n)
 
-# n="sinan"                        #  packing
+# n = (10,20,30,40,50)
+# print(n[0])                                         # Find First Element
+# print(n[-1])                                        # Find Last Element
+# print(n[2:4])                                       # Slicing
+
+
+# n1 = (1,2)                                          # Concatenation
+# n2 = (3,4)
+# print(n1 + n2)
+
+
+# n = (1,2)                                           # Repetition
+# print(n * 3)
+
+
+# n = (2,3)                                           # Membership
+# print(2 in n)
+# print(1 not in n)
+
+
+# n = (1,2,3,4,5)                                     # Length
+# print(len(n))
+
+
+# n = (1,2,2,3,4,5,2)                                 # Count (built-in Method)
+# print(n.count(2))
+
+
+# n = (19,20,30)                                      # Index (built-in Method)
+# print(n.index(20))
+
+
+
+# n="sinan"                                           # packing
 # a=19
 # p="India"
 # pack = n , a, p 
 # print(pack)
 
-# un = ("sinan",19,"India")        #  Unpacking
+
+
+# un = ("sinan",19,"India")                            # Unpacking
 # n, a, p = un
 # print(n)
 # print(a)
