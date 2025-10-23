99be6baf9979757f7b7412b01589c37ef3e4cf3b
sinan-prvt <mohamedsinan9400@gmail.com>
2025-10-23 11:06:27 +0530

    List-Comprehesion-added

---
+++ Comprehesion/ListComprehesion.py
@@ -0,0 +1,32 @@
+
+#--------------------------------- List Comprehension -------------------------------------------
+
+
+# number = [1,2,3,4,5,6]
+# square = [i * i for i in number]                                   # find square
+# print(square)
+
+
+
+# number = [1,2,3,4,5,6]                                              # With condition
+# even = [n for n in number if n % 2 == 0]
+# print(even)
+
+
+# result = ["Even" if n % 2 == 0 else "Odd" for n in range(5)]         # With if-else
+# print(result)
+
+
+
+# matrix = [[1,2,3],[4,5,6]]                                          # Nested Loop (Matrix Example)
+# flatterned = [num for row in matrix for num in row]
+# print(flatterned)
+
+
+
+# name = "python"                                                      # String Example
+# uppercase = [ch.upper() for ch in name]
+# print(uppercase)
+
+
+
