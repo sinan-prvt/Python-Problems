2cfada34c98b5159c5ae0c4c29c368f6f17b476f
sinan-prvt <mohamedsinan9400@gmail.com>
2025-10-24 09:40:37 +0530

    "Dictionary-Updated"

--- DataTypes/dict.py
+++ DataTypes/dict.py
@@ -1,5 +1,5 @@
 
-#------------------------------------------ Dicionary -------------------------------------------
+#------------------------------------------ Dictionary -------------------------------------------
 
 
 # person = {
@@ -8,21 +8,34 @@
 #     "place" : "Kottakkal" 
 # }
 
-# person["place"] = "Puthupparamba"         #  Updating
+
+# person["place"] = "Puthupparamba"                             #  Updating
 # print(person)
 
-# person["country"] = "India"               #  Adding
+
+# person["country"] = "India"                                   #  Adding
 # print(person)
 
-# person.pop("place")                       #  Removing
+
+# person.pop("place")                                           #  Removing
 # print(person)
 
-# print(person.keys())                      #  keys print
 
-# print(person.values())                    #  values print
+# print(person.keys())                                          #  keys print
+
+
+# print(person.values())                                        #  values print
+
+
+# fruits = {"Apple","Banana","Pinapple"}                         # Length and name
+# length = {fruit: len(fruit) for fruit in fruits}
+# print(length)
+
+
 
 # for key, value in person.items():         #  Looping
 #     print(key, ":" , value)
+
 
 # if "name" in person :                     #  Checking
 #     print("Yes it Is in it")
