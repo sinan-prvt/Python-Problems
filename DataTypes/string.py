80b8f1116885a3d82729418d7719c1f84c038d92
sinan-prvt <mohamedsinan9400@gmail.com>
2025-11-04 12:13:04 +0530

    "string-updated"

--- DataTypes/string.py
+++ DataTypes/string.py
@@ -1,3 +1,4 @@
+
 #------------------------------------------ String -------------------------------------------
 
 
@@ -5,33 +6,43 @@
 # n = ["Hello", "World" , "Python", "is", "Good"]            #  Join
 # print(" ".join(n))
 
+
 # n = "Sinan"                                                #  UpperCase
 # print(n.upper())
+
 
 # n = "sinan"                                                #  Length
 # print(len(n))
 
+
 # n = "SINAN"                                                #  LowerCase
 # print(n.lower())
+
 
 # n = "sinan"                                                #  Stating letter Capital
 # print(n.title())
 
+
 # n = "    Sinan    "                                        #  Starting and ending space removing
 # print(n.strip())
+
 
 # n = "I Love React"                                         #  Replacing item
 # print(n.replace("React", "Python"))
 
+
 # n = "One, Two, Three"                                      #  Spliting 
 # print(n.split(","))
+
 
 # n = "Sinan"                                                #  Startwith and Endwith checking
 # print(n.startswith("Si"))
 # print(n.endswith("an"))
 
+
 # n = "sinan"                                                #  How many Count items
 # print(n.count("n"))
+
 
 # n = "python21"                                             #  Check aphabet,number,sapce,lower,etc...
 # print(n.isalpha())
