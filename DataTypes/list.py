5cfba9c8f89816c0f63fd34685c2a06fb87b8dd6
sinan-prvt <mohamedsinan9400@gmail.com>
2025-11-04 11:09:10 +0530

    "list-updated"

--- DataTypes/list.py
+++ DataTypes/list.py
@@ -6,41 +6,52 @@
 # n.append("grape")                              #  Add to last
 # print(n)
 
+
 # n.insert(1,"orange")                            #  Add to any index
 # print(n)
+
 
 # n.extend(["kiwi","pappaya"])                     #  Add Multiple items
 # print(n)
 
+
 # n.remove("grape")                                #  Remove with name
 # print(n)
+
 
 # n.pop()                                          #  Remove last item 
 # print(n)
 
+
 # n.pop(2)                                          #  Removing using index
 # print(n)
+
 
 # n.sort()                                          #  Sorting items
 # print(n)
 
+
 # n.sort(reverse=True)                              #  Sorting in Reverse
 # print(n)
+
 
 # n.sorted()                                        #  return new sorted list without modifying new
 # print(n)
 
+
 # n.reverse()                                        #  Reversing items
 # print(n)
 
+
 # print(len(n))                                       #  Length of items
+
 
 # print(n.index("mango"))                             #  index number print
 
-# print(n.index("apple"))                             #  Count how may items 
 
 # for item in n :                                      #  Looping
 #     print(item)
+
 
 # if "apple" in n :                                    #  Checking
 #     print("Founded")
