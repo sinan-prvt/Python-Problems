8f17b3b525ac0c463999940fb49b298fc1ea42ee
sinan-prvt <mohamedsinan9400@gmail.com>
2026-03-12 11:27:04 +0530

    Added Real World Examples in comprehension

---
+++ Real Problems/Comprehension.py
@@ -0,0 +1,98 @@
+#---------------------- Real Programming Examples ---------------------------
+
+
+#================== Convert strings to uppercase
+
+
+# names = ["alice", "bob", "charlie"]
+
+# upper = [name.upper() for name in names]
+
+# print(upper)
+
+
+
+
+# ================== Filter positive numbers
+
+
+# num = [-1, 2, -3, 4, -5]
+
+# positive = [n for n in num if n > 0]
+
+# print(positive)
+
+
+
+
+#================== Create squares dictionary
+
+
+# num = [1, 2, 3, 4, 5]
+
+# square = {x : x * x for x in num}
+
+# print(square)
+
+
+
+
+#================== Flatten nested list
+
+
+# num = [[1, 2], [3, 4], [5, 6]]
+
+# flat = [n for sub in num for n in sub]
+
+# print(flat)
+
+
+
+
+# ================== From a list, extract only even numbers
+
+
+# num = [1, 2, 3, 4, 5, 6]
+
+# even = [n for n in num if n % 2 == 0]
+
+# print(even)
+
+
+
+
+# ================== Get lengths of words
+
+
+# words = ["apple", "banana", "watermelon"]
+
+# length = [len(w) for w in words]
+
+# print(length)
+
+
+
+
+# ================== Extract vowels from string
+
+
+# text = "Mohamed Sinan"
+
+# vowels = [ch for ch in text if ch in "AEIOUaeiou"]
+
+# print(vowels)
+
+
+
+
+# ================== Convert numbers: even → "even" odd → "odd"
+
+
+# num = [1, 2, 3, 4, 5]
+
+# result = ["even" if n % 2 == 0 else "odd" for n in num]
+
+# print(result)
+
+
+
