a9b7962e89e94bd2f04ccbf249c11a2274f05836
sinan-prvt <mohamedsinan9400@gmail.com>
2026-03-12 10:53:26 +0530

    Added Real World Examples with in List

---
+++ Real Problems/List.py
@@ -0,0 +1,122 @@
+#---------------------- Real Programming Examples ---------------------------
+
+
+#================== Sum of list
+
+
+# num = [1,2,3,4,5]
+
+# total = 0
+
+# for n in num:
+#     total += n
+
+# print(total)
+
+
+
+
+#================== Find largest number
+
+
+# num = [1,3,4,5,7,8]
+
+# largest = num[0]
+
+# for n in num:
+#     if n > largest:
+#         largest = n
+
+# print(largest)
+
+
+
+#================== Count how many items are in a list without using len()
+
+
+# num = [1,2,3,4,5]
+
+# count = 0
+
+# for n in num:
+#     count += 1
+
+# print(count)
+
+
+
+#================== Count Even Numbers
+
+
+# num = [1,2,3,4,5,6,7,8,9]
+
+# count = 0
+
+# for n in num:
+#     if n % 2 == 0:
+#         count += 1
+
+# print(count)
+
+
+
+#================== Sum of Even Numbers
+
+
+# num = [1,2,3,4,5,6,7,8,9]
+
+# total = 0
+
+# for n in num:
+#     if n % 2 == 0:
+#         total += n
+    
+# print(total)
+
+
+
+#================== Find Duplicate Values
+
+
+# num = [1,2,3,4,5,6,7,8,9,1,2]
+
+# duplicate = []
+
+# for n in num:
+#     if num.count(n) > 1 and n not in duplicate:
+#         duplicate.append(n)
+    
+# print(duplicate)
+
+
+
+# ================== Reverse a List
+
+
+# num = [1,2,3,4,5]
+
+# reversed_list = []
+
+# for n in num:
+#     reversed_list.insert(0, n)
+
+# print(reversed_list)
+
+
+
+# ================== Remove Negative Numbers
+
+
+# num = [1,-2,3,-4,5,-6]
+
+# positive = []
+
+# for n in num:
+#     if n >= 0:
+#         positive.append(n)
+    
+# print(positive)
+
+
+
+# ================== Find Common Elements
